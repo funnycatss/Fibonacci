5f5f433be35db9ed0380b1aa393726ac249004a1
funnycats <134oriflame@gmail.com>
2017-11-10 15:48:07 +0000

    Initial

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 
-alert(' 1 + 1 = 2');
-alert(' 1 + 2 = 3');
-alert(' 2 + 3 = 5');
-alert(' 3 + 5 = 8');
-alert(' 5 + 8 = 13');
-alert(' 8 + 13 = 21');
-alert(' 13 + 21 = 29');
-alert(' 21 + 29 = 50');
-alert(' 29 + 50 = 79');
-alert(' 50 + 79 = 129');
-alert('Кінець');
+//alert(' 1 + 1 = 2');
+//alert(' 1 + 2 = 3');
+//alert(' 2 + 3 = 5');
+//alert(' 3 + 5 = 8');
+//alert(' 5 + 8 = 13');
+//alert(' 8 + 13 = 21');
+//alert(' 13 + 21 = 29');
+//alert(' 21 + 29 = 50');
+//alert(' 29 + 50 = 79');
+//alert(' 50 + 79 = 129');
+//alert('Кінець');
+
+//var x1, x2;
+//x1=1;
+//x2=1;
+//alert(x1+x2);
+//var x2, x3;
+//x3=2;
+//alert(x2+x3);
+//var x3, x4;
+//x4=3;
+//alert(x3+x4);
+//var x4, x5;
+//x5=5;
+//alert(x4+x5);
+//var x5, x6;
+//x6=8;
+//alert(x5+x6);
+//var x6, x7;
+//x7=13;
+//alert(x6+x7);
+//var x6, x7;
+//x8=21;
+//alert(x7+x8);
+//var x7, x8;
+//x9=29;
+//alert(x8+x9);
+//var x8, x9;
+//x10=50;
+//alert(x9+x10);
+//var x10, x11;
+//x11=79;
+//alert(x10+x11);
+
+const f = Number(prompt(f));
+let fib = [ 1, 1];
+fib[1]=fib[i-1]+fib[i-2];
+
+document.write(result);
+ 
